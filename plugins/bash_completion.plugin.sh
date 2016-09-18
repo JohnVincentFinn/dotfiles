edbe7f35ee88af69105c76fdcdcc07e37294fc4d
John Finn <johnvincentfinn@gmail.com>
2016-09-18 08:37:49 -0700

    Old changes

--- plugins/bash_completion.plugin.sh
+++
@@ -1,9 +0,0 @@
-# adds certain bash completion stuff
-
-#source ~/.files/bashrc.d/.bash_completion/bash_completion
-
-# SSH auto-completion based on entries in known_hosts.
-if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq)" ssh scp sftp
-fi
-
